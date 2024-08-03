d7ecbe28ac4d1ee7ac71b5e04300b8ee928f8522
Pramod Mali <pramodmaali@gmail.com>
2024-08-03 18:47:27 +0530

    Add dev env file with omdb placeholder key

--- src/commons/api/api.ts
+++ src/commons/api/api.ts
@@ -1,4 +1,4 @@
-const API_KEY = "<your_key>";
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 type Method = "GET" | "POST" | "PUT" | "DELETE";
